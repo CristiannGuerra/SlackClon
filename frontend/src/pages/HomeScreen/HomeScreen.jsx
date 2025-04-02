b39823c2252ed1728cabc9b842c6f8eae7825175
Cristian <camguerra93@gmail.com>
2025-04-01 21:02:53 -0300

    Redirect-Added

--- frontend/src/pages/HomeScreen/HomeScreen.jsx
+++ frontend/src/pages/HomeScreen/HomeScreen.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import './HomeScreen.css'
 
 const HomeScreen = () => {
+  const token = sessionStorage.getItem('authorization_token')
+
+  if (!token) {
+    window.location.href = '/login'
+  } else {
+    window.location.href = '/login'
+  }
+
   return (
-    <div>HomeScreen</div>
+    <div className='home-screen'></div>
   )
 }
 
